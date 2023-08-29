2c7897671a4e083675e8b5a98caa427f282c8790
Ember Schryver <35340347+Bens0@users.noreply.github.com>
2023-08-29 16:21:40 -0400

    Tried to curl but didnt work

--- picofurby.c
+++ picofurby.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "lib/curl/curl.h"
 
 #define PROJECT_NAME "PicoFurby"
 
-int main(int argc, char **argv) {
-    if(argc != 1) {
-        printf("%s takes no arguments.\n", argv[0]);
-        return 1;
-    }
-    printf("This is project %s.\n", PROJECT_NAME);
-    return 0;
+
+int main() {
+  CURL *curl;
+  CURLcode res;
+  curl_global_init(CURL_GLOBAL_ALL);
+  CURL *curl_easy_init();
+  if(curl) {
+    CURLcode res;
+    // Set the API endpoint
+    curl_easy_setopt(curl, CURLOPT_URL, "https://api.openai.com/v1/chat/completions");
+    // Set the API key  
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+    headers = curl_slist_append(headers, "Authorization: Bearer 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");
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+    // Set the request data (input prompt and parameters)
+    const char *data = "{\"prompt\": \"Once upon a time\", \"max_tokens\": 50}";
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
+    // Perform the request
+    res = curl_easy_perform(curl);
+
+    if (res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+
+    curl_easy_cleanup(curl);
+}
+curl_global_cleanup();
+return 0;
 }
